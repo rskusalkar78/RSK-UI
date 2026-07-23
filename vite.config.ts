39e81dc09900c3b9e67ba9fcb2a3785dda178106
Rohan Shankar Kusalkar <rkusalkar78@gmail.com>
2026-07-23 20:00:30 +0530

    feat: initialize project with Vite library configuration and documentation

---
+++ vite.config.ts
@@ -0,0 +1,35 @@
+import { fileURLToPath } from 'url';
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
+
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'RskUi',
+      formats: ['es', 'umd'],
+      fileName: (format) => `rsk-ui.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom', 'react/jsx-runtime', 'framer-motion'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+          'react/jsx-runtime': 'jsxRuntime',
+          'framer-motion': 'Motion',
+        },
+      },
+    },
+  },
+});
