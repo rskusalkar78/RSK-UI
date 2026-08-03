66a3f379b800f3e3715ac3643f7fb5f81dfab226
Rohan Shankar Kusalkar <rkusalkar78@gmail.com>
2026-08-03 18:33:08 +0530

    feat: add Home, Hooks, and Icons pages

---
+++ docs/src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <article>
+      <h1>RSK UI</h1>
+      <p>Welcome to the RSK UI documentation.</p>
+    </article>
+  )
+}
